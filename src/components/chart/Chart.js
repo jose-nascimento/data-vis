e80c9c24ba2cfe120c386fed2f0dd20b934415dc
jose-nascimento <josevolponilicio@gmail.com>
2019-05-21 19:24:25 -0300

    Mudanças na viewBox

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -5,9 +5,10 @@ const style = {
   textAlign: 'center',
   color: '#EEE',
   backgroundColor: '#fff',
-  width: '70vw',
+  width: '640px',
+  height: '640px',
   margin: '0 auto',
-  marginTop: '10rem',
+  height: 'auto',
 };
 
 class Chart extends Component {
@@ -15,25 +16,33 @@ class Chart extends Component {
     width: 640,
     height: 640,
     margin: {
-        top: 25,
-        right: 25,
-        bottom: 25,
-        left: 25,
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 20,
     },
     group: (props) => <g>{props.children}</g>,
   };
   render() {
-    // const { width, height, margin, group: Group, ...props } = { ...this.defaultProps, ...this.props };
+    const { width, height, margin, group: Group, ...props } = { ...this.props };
     return (
       <figure style={style}>
-          <svg>
-              <this.props.group>
-                  {this.props.children}
-              </this.props.group>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox={`${-margin.left} ${-margin.top} ${width -
+            margin.left - margin.right} ${height - margin.top - margin.bottom}`}
+          width='100%'
+          height='auto'
+        >
+          <svg x="320" y="320">
+            <circle cx="64" cy="64" r="64" fill="#000" />
           </svg>
+        </svg>
       </figure>
-    )
+    );
   }
 }
-
+//{`${width}`}
+//{`${height}`}
 export default Chart;
